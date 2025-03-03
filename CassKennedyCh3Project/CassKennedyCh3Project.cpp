0c6770ae23a991cb934284d785f4ba9497ce74ba
Cass-Kennedy <alkennedy228@gmail.com>
2025-03-03 13:29:50 -0500

    Added clarifying comments.

--- CassKennedyCh3Project/CassKennedyCh3Project.cpp
+++ CassKennedyCh3Project/CassKennedyCh3Project.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 int main()
 {
+    //The next four lines give the initial amounts given by the recipe, in cookies or cups.
     float initialAmount = 48.0;
     float initialSugar = 1.5;
     float initialButter = 1.0;
     float initialFlour = 2.75;
-    //The initial amounts given by the recipe, in cookies or cups.
-    int newAmount;
+    int newAmount; //The amount of cookies the user wants to make
     cout << "How many cookies would you like to make?";
     cin >> newAmount;
+    float adjustmentProportion = newAmount / initialAmount; //The ratio by which the initial recipe needs to be adjusted
     cout << "To make " << newAmount << " cookies, you need " << initialSugar * (newAmount / initialAmount) << " cups of sugar, " << initialButter * (newAmount / initialAmount) << " cups of butter, and " << initialFlour * (newAmount / initialAmount) << " cups of flour.";
+    //The display what someone needs for different amounts of cookies.
     return 0;
 }
