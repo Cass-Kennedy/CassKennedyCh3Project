0aab6cf8094a1f658705e5f780b4d58d677e1f7b
Cass-Kennedy <alkennedy228@gmail.com>
2025-03-03 13:14:14 -0500

    Made a program to calculate and display the information about interest generated.

--- CassKennedyCh3Project/CassKennedyCh3Project.cpp
+++ CassKennedyCh3Project/CassKennedyCh3Project.cpp
@@ -1,16 +1,26 @@
-//This branch of the program is designed to help the user adjust the amount of ingredients when changing the amount of a recipe.
+//This branch of the program is designed to help the user calculate the amount of money they will get from interest in a year
 #include <iostream>
 using namespace std;
 int main()
 {
-    float initialAmount = 48.0;
-    float initialSugar = 1.5;
-    float initialButter = 1.0;
-    float initialFlour = 2.75;
-    //The initial amounts given by the recipe, in cookies or cups.
-    int newAmount;
-    cout << "How many cookies would you like to make?";
-    cin >> newAmount;
-    cout << "To make " << newAmount << " cookies, you need " << initialSugar * (newAmount / initialAmount) << " cups of sugar, " << initialButter * (newAmount / initialAmount) << " cups of butter, and " << initialFlour * (newAmount / initialAmount) << " cups of flour.";
+    float principal; //initial investment
+    float interestRate; //account's interest rate
+    float compoundTime; //number of times it compounds per year
+    //The next six lines get the user to input data
+    cout << "Enter your initial investment";
+        cin >> principal;
+    cout << "Enter your interest rate";
+        cin >> interestRate;
+    cout << "Enter the amount of times your account compounds in a year";
+        cin >> compoundTime;
+    //The next two lines calulate how much interest is generated and how much money is in the account
+    float interestGained = pow((1 + (interestRate / compoundTime)), compoundTime);
+    float overallAmount = principal+interestGained;
+    //The next five lines display the report of information
+    cout << "Principal: " << principal << endl;
+    cout << "Interest rate: " << interestRate << endl;
+    cout << "Compounds per year: " << compoundTime << endl;
+    cout << "Interest gained: " << interestGained << endl;
+    cout << "Overall money in your account: " << overallAmount << endl;
     return 0;
 }
